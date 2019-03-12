b0435112452a32b3672abfa12410dbfc205cf818
Zac Sweers <pandanomic@gmail.com>
2019-03-12 00:51:05 -0700

    Update for kotlin 1.3.21
    No inline class support but at least seems simpler than before

--- redacted-compiler-plugin/src/main/kotlin/io/sweers/redacted/compiler/RedactedCodegenExtension.kt
+++ redacted-compiler-plugin/src/main/kotlin/io/sweers/redacted/compiler/RedactedCodegenExtension.kt
@@ -1,17 +1,17 @@
 package io.sweers.redacted.compiler
 
 import io.sweers.redacted.annotation.Redacted
+import org.jetbrains.annotations.NotNull
 import org.jetbrains.kotlin.cli.common.messages.CompilerMessageLocation
-import org.jetbrains.kotlin.cli.common.messages.CompilerMessageSeverity.ERROR
+import org.jetbrains.kotlin.cli.common.messages.CompilerMessageSeverity
 import org.jetbrains.kotlin.cli.common.messages.CompilerMessageSeverity.WARNING
 import org.jetbrains.kotlin.cli.common.messages.MessageCollector
-import org.jetbrains.kotlin.codegen.AsmUtil.correctElementType
-import org.jetbrains.kotlin.codegen.AsmUtil.genInvokeAppendMethod
-import org.jetbrains.kotlin.codegen.AsmUtil.genStringBuilderConstructor
-import org.jetbrains.kotlin.codegen.ExpressionCodegen
-import org.jetbrains.kotlin.codegen.FunctionGenerationStrategy.CodegenBased
+import org.jetbrains.kotlin.codegen.AsmUtil
+import org.jetbrains.kotlin.codegen.ClassBuilder
+import org.jetbrains.kotlin.codegen.FunctionCodegen
 import org.jetbrains.kotlin.codegen.ImplementationBodyCodegen
-import org.jetbrains.kotlin.codegen.JvmCodegenUtil.couldUseDirectAccessToProperty
+import org.jetbrains.kotlin.codegen.JvmKotlinType
+import org.jetbrains.kotlin.codegen.OwnerKind.ERASED_INLINE_CLASS
 import org.jetbrains.kotlin.codegen.context.FieldOwnerContext
 import org.jetbrains.kotlin.codegen.context.MethodContext
 import org.jetbrains.kotlin.codegen.extensions.ExpressionCodegenExtension
@@ -25,15 +25,17 @@ import org.jetbrains.kotlin.descriptors.impl.referencedProperty
 import org.jetbrains.kotlin.incremental.components.NoLookupLocation.WHEN_GET_ALL_DESCRIPTORS
 import org.jetbrains.kotlin.name.FqName
 import org.jetbrains.kotlin.name.Name
+import org.jetbrains.kotlin.psi.KtClassOrObject
 import org.jetbrains.kotlin.resolve.BindingContext
 import org.jetbrains.kotlin.resolve.descriptorUtil.annotationClass
+import org.jetbrains.kotlin.resolve.descriptorUtil.builtIns
 import org.jetbrains.kotlin.resolve.descriptorUtil.fqNameSafe
-import org.jetbrains.kotlin.resolve.jvm.AsmTypes.JAVA_STRING_TYPE
-import org.jetbrains.kotlin.resolve.jvm.diagnostics.JvmDeclarationOrigin
-import org.jetbrains.kotlin.resolve.jvm.diagnostics.JvmDeclarationOriginKind
-import org.jetbrains.kotlin.resolve.jvm.jvmSignature.JvmMethodSignature
+import org.jetbrains.kotlin.resolve.jvm.AsmTypes
+import org.jetbrains.kotlin.resolve.jvm.diagnostics.OtherOrigin
+import org.jetbrains.kotlin.resolve.substitutedUnderlyingType
+import org.jetbrains.org.objectweb.asm.AnnotationVisitor
+import org.jetbrains.org.objectweb.asm.Opcodes
 import org.jetbrains.org.objectweb.asm.Type
-import org.jetbrains.org.objectweb.asm.Type.getObjectType
 import org.jetbrains.org.objectweb.asm.commons.InstructionAdapter
 
 /**
@@ -55,7 +57,7 @@ class RedactedCodegenExtension(
     log("Reading ${targetClass.name}")
     if (targetClass.isData) {
       log("Not a data class")
-      messageCollector.report(ERROR,
+      messageCollector.report(CompilerMessageSeverity.ERROR,
           "Redacted is not supported on data classes!",
           // I don't know how to get a location from a descriptor :(
           CompilerMessageLocation.create(null))
@@ -70,7 +72,7 @@ class RedactedCodegenExtension(
         .filter {
           log("Reading param ${it.name}")
           log("Property is ${it.referencedProperty}")
-          log("Annotations are ${it.annotations.getAllAnnotations().joinToString { it.annotation.fqName.toString() }}")
+//          log("Annotations are ${it.annotations.getAllAnnotations().joinToString { it.annotation.fqName.toString() }}")
           log("Property annotations ${it.referencedProperty?.annotations?.joinToString { it.annotationClass?.fqNameSafe.toString() }}")
           it.isRedacted
         }
@@ -84,115 +86,166 @@ class RedactedCodegenExtension(
         .map { it to it.isRedacted }
     log("Found params: ${finalProperties.joinToString { it.first.name.asString() }}")
 
-    targetClass.writeToStringFunction(codegen, finalProperties)
-  }
-
-  private fun ClassDescriptor.writeToStringFunction(
-      codegen: ImplementationBodyCodegen,
-      propertiesToSerialize: List<Pair<PropertyDescriptor, Boolean>>
-  ): Unit? {
-    log("Writing toString()")
-
-    return findToStringFunction()?.write(codegen) {
-      genStringBuilderConstructor(v)
-      var first = true
-      for ((propertyDescriptor, isRedacted) in propertiesToSerialize) {
-        val possibleValue = if (isRedacted) "\"██\"" else ""
-        if (isRedacted) {
-          log("Writing redacted property ${propertyDescriptor.name}")
-        }
-        if (first) {
-          v.aconst("$name(${propertyDescriptor.name.asString()}=$possibleValue")
-          first = false
-        } else {
-          v.aconst(", ${propertyDescriptor.name.asString()}=$possibleValue")
-        }
-        genInvokeAppendMethod(v, JAVA_STRING_TYPE)
-
-        if (!isRedacted) {
-          log("Writing non-redacted property ${propertyDescriptor.name}")
-          var type = genPropertyOnStack(state,
-              state.typeMapper,
-              v,
-              context,
-              propertyDescriptor,
-              /*copied from descriptor.classAsmType*/
-              getObjectType(state.typeMapper.classInternalName(this@writeToStringFunction)),
-              0)
-          if (type.sort == Type.ARRAY) {
-            val elementType = correctElementType(type)
-            if (elementType.sort == Type.OBJECT || elementType.sort == Type.ARRAY) {
-              v.invokestatic(
-                  "java/util/Arrays",
-                  "toString",
-                  "([Ljava/lang/Object;)Ljava/lang/String;",
-                  false
-              )
-              type = JAVA_STRING_TYPE
-            } else {
-              if (elementType.sort != Type.CHAR) {
-                v.invokestatic(
-                    "java/util/Arrays",
-                    "toString",
-                    "(${type.descriptor})Ljava/lang/String;",
-                    false
-                )
-                type = JAVA_STRING_TYPE
-              }
-            }
-          }
-          genInvokeAppendMethod(v, type)
-        }
-      }
-
-      v.aconst(")")
-      genInvokeAppendMethod(v, JAVA_STRING_TYPE)
-
-      v.invokevirtual("java/lang/StringBuilder", "toString", "()Ljava/lang/String;", false)
-      v.areturn(JAVA_STRING_TYPE)
-    }
-  }
-
-  private fun genPropertyOnStack(
-      state: GenerationState,
-      typeMapper: KotlinTypeMapper,
-      iv: InstructionAdapter,
-      context: MethodContext,
-      propertyDescriptor: PropertyDescriptor,
-      classAsmType: Type,
-      index: Int
-  ): Type {
-    iv.load(index, classAsmType)
-    return if (couldUseDirectAccessToProperty(
-            propertyDescriptor,
-            true,
-            false,
-            context,
-            state.shouldInlineConstVals
-        )) {
-      val type = typeMapper.mapType(propertyDescriptor.type)
-      val fieldName = (context.parentContext as FieldOwnerContext).getFieldName(
-          propertyDescriptor, false)
-      iv.getfield(classAsmType.internalName, fieldName, type.descriptor)
-      type
-    } else {
-      val method = typeMapper.mapAsmMethod(propertyDescriptor.getter!!)
-      iv.invokevirtual(classAsmType.internalName, method.name, method.descriptor, false)
-      method.returnType
-    }
+    ToStringGenerator(
+        declaration = codegen.myClass as KtClassOrObject,
+        classDescriptor = targetClass,
+        classAsmType = codegen.typeMapper.mapType(targetClass),
+        fieldOwnerContext = codegen.context,
+        v = codegen.v,
+        generationState = codegen.state
+    ).generateToStringMethod(
+        targetClass.findToStringFunction()!!,
+        properties
+    )
   }
 }
 
-private fun FunctionDescriptor.write(
-    codegen: ImplementationBodyCodegen,
-    code: ExpressionCodegen.() -> Unit) {
-  val declarationOrigin = JvmDeclarationOrigin(JvmDeclarationOriginKind.OTHER, null, this)
-  // Red in the IDE but they do exist! MemberCodegen has state and functionCodegen
-  codegen.functionCodegen.generateMethod(declarationOrigin, this,
-      object : CodegenBased(codegen.state) {
-        override fun doGenerateBody(e: ExpressionCodegen, signature: JvmMethodSignature) =
-            e.code()
-      })
+private class ToStringGenerator(
+    private val declaration: KtClassOrObject,
+    private val classDescriptor: ClassDescriptor,
+    private val classAsmType: Type,
+    private val fieldOwnerContext: FieldOwnerContext<*>,
+    private val v: ClassBuilder,
+    private val generationState: GenerationState
+) {
+  private val typeMapper: KotlinTypeMapper = generationState.typeMapper
+  private val underlyingType: JvmKotlinType
+
+  private val toStringDesc: String
+    get() = "($firstParameterDesc)Ljava/lang/String;"
+
+  private val firstParameterDesc: String
+    get() {
+      return if (fieldOwnerContext.contextKind == ERASED_INLINE_CLASS) {
+        underlyingType.type
+            .descriptor
+      } else {
+        ""
+      }
+    }
+
+  private val access: Int
+    get() {
+      var access = Opcodes.ACC_PUBLIC
+      if (fieldOwnerContext.contextKind == ERASED_INLINE_CLASS) {
+        access = access or Opcodes.ACC_STATIC
+      }
+
+      return access
+    }
+
+  init {
+    this.underlyingType = JvmKotlinType(
+        typeMapper.mapType(classDescriptor),
+        classDescriptor.defaultType.substitutedUnderlyingType())
+  }
+
+  fun generateToStringMethod(function: FunctionDescriptor,
+      properties: List<PropertyDescriptor>) {
+    val context = fieldOwnerContext.intoFunction(function)
+    val methodOrigin = OtherOrigin(function)
+    val toStringMethodName = mapFunctionName(function)
+    val mv = v.newMethod(methodOrigin, access, toStringMethodName, toStringDesc, null, null)
+
+    if (fieldOwnerContext.contextKind != ERASED_INLINE_CLASS && classDescriptor.isInline) {
+      FunctionCodegen.generateMethodInsideInlineClassWrapper(
+          methodOrigin,
+          function,
+          classDescriptor,
+          mv,
+          typeMapper)
+      return
+    }
+
+    visitEndForAnnotationVisitor(
+        mv.visitAnnotation(Type.getDescriptor(NotNull::class.java), false))
+
+    if (!generationState.classBuilderMode.generateBodies) {
+      FunctionCodegen.endVisit(mv, toStringMethodName, declaration)
+      return
+    }
+
+    val iv = InstructionAdapter(mv)
+
+    mv.visitCode()
+    AsmUtil.genStringBuilderConstructor(iv)
+
+    var first = true
+    for (propertyDescriptor in properties) {
+      val isRedacted = propertyDescriptor.isRedacted
+      val possibleValue = if (isRedacted) "\"██\"" else ""
+      if (first) {
+        iv.aconst(classDescriptor.name.toString() + "(" + propertyDescriptor.name
+            .asString() + "=$possibleValue")
+        first = false
+      } else {
+        iv.aconst(", " + propertyDescriptor.name
+            .asString() + "=$possibleValue")
+      }
+      AsmUtil.genInvokeAppendMethod(iv, AsmTypes.JAVA_STRING_TYPE, null)
+
+      if (!isRedacted) {
+        val type = genOrLoadOnStack(iv, context, propertyDescriptor, 0)
+        var asmType = type.type
+        var kotlinType = type.kotlinType
+
+        if (asmType.sort == Type.ARRAY) {
+          val elementType = AsmUtil.correctElementType(asmType)
+          if (elementType.sort == Type.OBJECT || elementType.sort == Type.ARRAY) {
+            iv.invokestatic("java/util/Arrays",
+                "toString",
+                "([Ljava/lang/Object;)Ljava/lang/String;",
+                false)
+            asmType = AsmTypes.JAVA_STRING_TYPE
+            kotlinType = function.builtIns
+                .stringType
+          } else if (elementType.sort != Type.CHAR) {
+            iv.invokestatic("java/util/Arrays",
+                "toString",
+                "(" + asmType.descriptor + ")Ljava/lang/String;",
+                false)
+            asmType = AsmTypes.JAVA_STRING_TYPE
+            kotlinType = function.builtIns
+                .stringType
+          }
+        }
+        AsmUtil.genInvokeAppendMethod(iv, asmType, kotlinType, typeMapper)
+      }
+    }
+
+    iv.aconst(")")
+    AsmUtil.genInvokeAppendMethod(iv, AsmTypes.JAVA_STRING_TYPE, null)
+
+    iv.invokevirtual("java/lang/StringBuilder", "toString", "()Ljava/lang/String;", false)
+    iv.areturn(AsmTypes.JAVA_STRING_TYPE)
+
+    FunctionCodegen.endVisit(mv, toStringMethodName, declaration)
+  }
+
+  private fun mapFunctionName(functionDescriptor: FunctionDescriptor): String {
+    return typeMapper.mapFunctionName(functionDescriptor, fieldOwnerContext.contextKind)
+  }
+
+  private fun visitEndForAnnotationVisitor(annotation: AnnotationVisitor?) {
+    annotation?.visitEnd()
+  }
+
+  private fun genOrLoadOnStack(iv: InstructionAdapter,
+      context: MethodContext,
+      propertyDescriptor: PropertyDescriptor,
+      index: Int): JvmKotlinType {
+    return if (fieldOwnerContext.contextKind == ERASED_INLINE_CLASS) {
+      iv.load(index, underlyingType.type)
+      underlyingType
+    } else {
+      ImplementationBodyCodegen.genPropertyOnStack(iv,
+          context,
+          propertyDescriptor,
+          classAsmType,
+          index,
+          generationState)
+    }
+  }
 }
 
 private fun ClassDescriptor.findToStringFunction(): SimpleFunctionDescriptor? {
