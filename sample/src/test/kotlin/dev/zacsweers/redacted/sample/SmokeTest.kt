63d4aa0116656aa1f4d936c608a5ad7fa4d83218
Zac Sweers <zsweers@slack-corp.com>
2020-01-23 16:50:07 -0500

    Add a proper test

---
+++ sample/src/test/kotlin/dev/zacsweers/redacted/sample/SmokeTest.kt
@@ -0,0 +1,12 @@
+package dev.zacsweers.redacted.sample
+
+import com.google.common.truth.Truth.assertThat
+import org.junit.Test
+
+class SmokeTest {
+  @Test
+  fun person() {
+    val person = Person("John Doe", "123-456-7890")
+    assertThat(person.toString()).isEqualTo("Person(name=John Doe, ssn=\"██\")")
+  }
+}
