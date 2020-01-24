586faf134b345c9794708f63df82ee09c3c441ac
Zac Sweers <zsweers@slack-corp.com>
2020-01-24 18:50:30 -0500

    Add annotations artifact

--- sample/src/main/kotlin/dev/zacsweers/redacted/sample/Redacted.kt
+++
@@ -1,9 +0,0 @@
-package dev.zacsweers.redacted.sample
-
-import kotlin.annotation.AnnotationRetention.BINARY
-import kotlin.annotation.AnnotationTarget.CLASS
-import kotlin.annotation.AnnotationTarget.PROPERTY
-
-@Retention(BINARY)
-@Target(PROPERTY, CLASS)
-annotation class Redacted
--- sample/src/main/kotlin/dev/zacsweers/redacted/sample/User.kt
+++ sample/src/main/kotlin/dev/zacsweers/redacted/sample/User.kt
@@ -1,5 +1,7 @@
 package dev.zacsweers.redacted.sample
 
+import dev.zacsweers.redacted.annotations.Redacted
+
 data class User(
     val name: String,
     @Redacted val phoneNumber: String
