ce420a13e457a4da551ca1cc42dc40cf7c87f12d
Zac Sweers <pandanomic@gmail.com>
2020-11-27 20:59:47 -0500

    Support StringConcatFactory (#32)
    * Update android deps
    
    * Enable string-concat on JDK9+
    
    * Fully use generator for stringconcatfactory support

--- redacted-compiler-plugin/src/main/kotlin/dev/zacsweers/redacted/compiler/RedactedCodegenExtension.kt
+++ redacted-compiler-plugin/src/main/kotlin/dev/zacsweers/redacted/compiler/RedactedCodegenExtension.kt
@@ -6,12 +6,14 @@ import org.jetbrains.kotlin.cli.common.messages.CompilerMessageSeverity
 import org.jetbrains.kotlin.cli.common.messages.CompilerMessageSeverity.LOGGING
 import org.jetbrains.kotlin.cli.common.messages.MessageCollector
 import org.jetbrains.kotlin.cli.common.messages.MessageUtil
-import org.jetbrains.kotlin.codegen.AsmUtil
+import org.jetbrains.kotlin.codegen.AsmUtil.correctElementType
+import org.jetbrains.kotlin.codegen.AsmUtil.genInvokeAppendMethod
 import org.jetbrains.kotlin.codegen.ClassBuilder
 import org.jetbrains.kotlin.codegen.FunctionCodegen
 import org.jetbrains.kotlin.codegen.ImplementationBodyCodegen
 import org.jetbrains.kotlin.codegen.JvmKotlinType
 import org.jetbrains.kotlin.codegen.OwnerKind.ERASED_INLINE_CLASS
+import org.jetbrains.kotlin.codegen.StackValue
 import org.jetbrains.kotlin.codegen.StringConcatGenerator
 import org.jetbrains.kotlin.codegen.context.FieldOwnerContext
 import org.jetbrains.kotlin.codegen.context.MethodContext
@@ -31,11 +33,13 @@ import org.jetbrains.kotlin.name.Name
 import org.jetbrains.kotlin.psi.KtClassOrObject
 import org.jetbrains.kotlin.resolve.BindingContext
 import org.jetbrains.kotlin.resolve.descriptorUtil.annotationClass
+import org.jetbrains.kotlin.resolve.descriptorUtil.builtIns
 import org.jetbrains.kotlin.resolve.descriptorUtil.fqNameSafe
-import org.jetbrains.kotlin.resolve.jvm.AsmTypes
+import org.jetbrains.kotlin.resolve.jvm.AsmTypes.JAVA_STRING_TYPE
 import org.jetbrains.kotlin.resolve.jvm.diagnostics.OtherOrigin
 import org.jetbrains.kotlin.resolve.source.getPsi
 import org.jetbrains.kotlin.resolve.substitutedUnderlyingType
+import org.jetbrains.kotlin.types.KotlinType
 import org.jetbrains.org.objectweb.asm.AnnotationVisitor
 import org.jetbrains.org.objectweb.asm.Opcodes
 import org.jetbrains.org.objectweb.asm.Type
@@ -128,13 +132,14 @@ private class ToStringGenerator(
 ) {
   private val typeMapper: KotlinTypeMapper = generationState.typeMapper
   private val underlyingType: JvmKotlinType
+  private val isInErasedInlineClass = fieldOwnerContext.contextKind == ERASED_INLINE_CLASS
 
   private val toStringDesc: String
     get() = "($firstParameterDesc)Ljava/lang/String;"
 
   private val firstParameterDesc: String
     get() {
-      return if (fieldOwnerContext.contextKind == ERASED_INLINE_CLASS) {
+      return if (isInErasedInlineClass) {
         underlyingType.type
             .descriptor
       } else {
@@ -145,7 +150,7 @@ private class ToStringGenerator(
   private val access: Int
     get() {
       var access = Opcodes.ACC_PUBLIC
-      if (fieldOwnerContext.contextKind == ERASED_INLINE_CLASS) {
+      if (isInErasedInlineClass) {
         access = access or Opcodes.ACC_STATIC
       }
 
@@ -164,7 +169,6 @@ private class ToStringGenerator(
     val toStringMethodName = mapFunctionName(function)
     val mv = v.newMethod(methodOrigin, access, toStringMethodName, toStringDesc, null, null)
 
-    val isInErasedInlineClass = fieldOwnerContext.contextKind == ERASED_INLINE_CLASS
     if (!isInErasedInlineClass && classDescriptor.isInline) {
       FunctionCodegen.generateMethodInsideInlineClassWrapper(
           methodOrigin,
@@ -187,65 +191,59 @@ private class ToStringGenerator(
 
     val iv = InstructionAdapter(mv)
 
-    val concatGenerator = StringConcatGenerator.create(generationState, iv)
+    val generator = StringConcatGenerator.create(generationState, iv)
     mv.visitCode()
-    concatGenerator.genStringBuilderConstructorIfNeded()
+    generator.genStringBuilderConstructorIfNeded()
 
     if (properties.isEmpty()) {
       // This is a redacted class, so just emit a single replacementString
       iv.aconst(classDescriptor.name.toString() + "(" + replacementString)
-      concatGenerator.invokeAppend(AsmTypes.JAVA_STRING_TYPE)
+      generator.invokeAppend(JAVA_STRING_TYPE)
     } else {
       var first = true
       for (propertyDescriptor in properties) {
         val isRedacted = propertyDescriptor.isRedacted(fqRedactedAnnotation)
         val possibleValue = if (isRedacted) replacementString else ""
         if (first) {
-          iv.aconst(classDescriptor.name.toString() + "(" + propertyDescriptor.name
-              .asString() + "=$possibleValue")
+          generator.addStringConstant(
+              "${classDescriptor.name}(${propertyDescriptor.name.asString()}=$possibleValue")
           first = false
         } else {
-          iv.aconst(", " + propertyDescriptor.name
-              .asString() + "=$possibleValue")
+          generator.addStringConstant(", ${propertyDescriptor.name.asString()}=$possibleValue")
         }
-        concatGenerator.invokeAppend(AsmTypes.JAVA_STRING_TYPE)
 
         if (!isRedacted) {
           val type = genOrLoadOnStack(iv, context, propertyDescriptor, 0)
           var asmType = type.type
+          var kotlinType: KotlinType? = type.kotlinType
 
           if (asmType.sort == Type.ARRAY) {
-            val elementType = AsmUtil.correctElementType(asmType)
+            val elementType = correctElementType(asmType)
             if (elementType.sort == Type.OBJECT || elementType.sort == Type.ARRAY) {
-              iv.invokestatic("java/util/Arrays",
-                  "toString",
-                  "([Ljava/lang/Object;)Ljava/lang/String;",
-                  false)
-              asmType = AsmTypes.JAVA_STRING_TYPE
+              iv.invokestatic("java/util/Arrays", "toString",
+                  "([Ljava/lang/Object;)Ljava/lang/String;", false)
+              asmType = JAVA_STRING_TYPE
+              kotlinType = function.builtIns.stringType
             } else if (elementType.sort != Type.CHAR) {
-              iv.invokestatic("java/util/Arrays",
-                  "toString",
-                  "(" + asmType.descriptor + ")Ljava/lang/String;",
-                  false)
-              asmType = AsmTypes.JAVA_STRING_TYPE
+              iv.invokestatic("java/util/Arrays", "toString",
+                  "(" + asmType.descriptor + ")Ljava/lang/String;", false)
+              asmType = JAVA_STRING_TYPE
+              kotlinType = function.builtIns.stringType
             }
           }
-          concatGenerator.invokeAppend(asmType)
+          genInvokeAppendMethod(generator, asmType, kotlinType, typeMapper, StackValue.onStack(asmType))
         }
       }
     }
 
-    iv.aconst(")")
-    concatGenerator.invokeAppend(AsmTypes.JAVA_STRING_TYPE)
+    generator.addStringConstant(")")
 
-    iv.invokevirtual("java/lang/StringBuilder", "toString", "()Ljava/lang/String;", false)
-    iv.areturn(AsmTypes.JAVA_STRING_TYPE)
+    generator.genToString()
+    iv.areturn(JAVA_STRING_TYPE)
 
     FunctionCodegen.endVisit(mv, toStringMethodName, declaration)
 
-    if (isInErasedInlineClass) {
-      recordMethodForFunctionIfRequired(function, toStringMethodName, toStringDesc)
-    }
+    recordMethodForFunctionIfRequired(function, toStringMethodName, toStringDesc)
   }
 
   private fun recordMethodForFunctionIfRequired(
@@ -253,8 +251,10 @@ private class ToStringGenerator(
       name: String,
       desc: String
   ) {
-    v.serializationBindings.put(JvmSerializationBindings.METHOD_FOR_FUNCTION, function, Method(name,
-        desc))
+    if (isInErasedInlineClass) {
+      v.serializationBindings.put(JvmSerializationBindings.METHOD_FOR_FUNCTION, function, Method(name,
+          desc))
+    }
   }
 
   private fun mapFunctionName(functionDescriptor: FunctionDescriptor): String {
