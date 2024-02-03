e8609ffb5de26be36c9fc6323669f0d289557690
Zac Sweers <pandanomic@gmail.com>
2024-02-03 16:20:25 -0500

    Fix RedactedOrigin

--- redacted-compiler-plugin/src/main/kotlin/dev/zacsweers/redacted/compiler/RedactedOrigin.kt
+++ redacted-compiler-plugin/src/main/kotlin/dev/zacsweers/redacted/compiler/RedactedOrigin.kt
@@ -15,6 +15,7 @@
  */
 package dev.zacsweers.redacted.compiler
 
+import org.jetbrains.kotlin.ir.declarations.IrDeclarationOrigin
 import org.jetbrains.kotlin.ir.declarations.IrDeclarationOriginImpl
 
-internal object RedactedOrigin : IrDeclarationOriginImpl("GENERATED_REDACTED_CLASS_MEMBER")
+internal val RedactedOrigin: IrDeclarationOrigin = IrDeclarationOriginImpl("GENERATED_REDACTED_CLASS_MEMBER")
