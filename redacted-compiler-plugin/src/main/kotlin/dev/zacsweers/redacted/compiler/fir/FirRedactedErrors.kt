fddd704dd6529109e83b36c7ca8d3cc3a893bead
Zac Sweers <pandanomic@gmail.com>
2024-04-29 17:19:50 -0400

    Wire up new FIR errors

--- redacted-compiler-plugin/src/main/kotlin/dev/zacsweers/redacted/compiler/fir/FirRedactedErrors.kt
+++ redacted-compiler-plugin/src/main/kotlin/dev/zacsweers/redacted/compiler/fir/FirRedactedErrors.kt
@@ -32,6 +32,10 @@ internal object FirRedactedErrors {
   val REDACTED_ON_NON_DATA_OR_VALUE_CLASS_ERROR by error0<PsiElement>(NAME_IDENTIFIER)
   val REDACTED_ON_VALUE_CLASS_PROPERTY_ERROR by error0<PsiElement>(NAME_IDENTIFIER)
   val CUSTOM_TO_STRING_IN_REDACTED_CLASS_ERROR by error0<PsiElement>(NAME_IDENTIFIER)
+  val UNREDACTED_ON_OBJECT_ERROR by error0<PsiElement>(NAME_IDENTIFIER)
+  val UNREDACTED_AND_REDACTED_ERROR by error0<PsiElement>(NAME_IDENTIFIER)
+  val UNREDACTED_ON_NONREDACTED_SUBTYPE_ERROR by error0<PsiElement>(NAME_IDENTIFIER)
+  val UNREDACTED_ON_NON_PROPERTY by error0<PsiElement>(NAME_IDENTIFIER)
 
   init {
     RootDiagnosticRendererFactory.registerFactory(FirRedactedErrorMessages)
@@ -62,5 +66,15 @@ private object FirRedactedErrorMessages : BaseDiagnosticRendererFactory() {
         FirRedactedErrors.REDACTED_ON_ENUM_CLASS_ERROR,
         ErrorMessages.REDACTED_ON_ENUM_CLASS_ERROR,
       )
+      put(FirRedactedErrors.UNREDACTED_ON_OBJECT_ERROR, ErrorMessages.UNREDACTED_ON_OBJECT_ERROR)
+      put(
+        FirRedactedErrors.UNREDACTED_AND_REDACTED_ERROR,
+        ErrorMessages.UNREDACTED_AND_REDACTED_ERROR,
+      )
+      put(
+        FirRedactedErrors.UNREDACTED_ON_NONREDACTED_SUBTYPE_ERROR,
+        ErrorMessages.UNREDACTED_ON_NONREDACTED_SUBTYPE_ERROR,
+      )
+      put(FirRedactedErrors.UNREDACTED_ON_NON_PROPERTY, ErrorMessages.UNREDACTED_ON_NON_PROPERTY)
     }
 }
